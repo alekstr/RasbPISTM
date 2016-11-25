f7ca35ede8c8da1013756e7088452a6143fd3901
Aleksej Tr <info@aleks.bz>
2016-11-25 22:08:34 +0000

    Added new led

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ require('shelljs/global');
 //console.log(udev.list()); // this is a long list :)
 var cdr = __dirname;
 
-var led = new gpio.Led(4);
+var statusLed = new gpio.Led(4);
+var errorLed = new gpio.Led(18);
+
+var reset = function() {
+	console.log('Reset led');
+	errorLed.set(0);
+	statusLed.set(0);
+}
+
+var error = function(msg) {
+	statusLed.set(0);
+	errorLed.set(1);
+	console.log(msg);
+};
+
+var working = function() {
+	statusLed.set(1);
+};
+
+var success = function(msg) {
+	errorLed.set(0);
+	statusLed.blink(8, 250);
+	console.log(msg);
+};
 
 var button = new gpio.Button(26, {activeLow: false});
 button.onClick(function(state){
@@ -21,14 +44,23 @@ var deviceTrack = function(action, device) {
 		console.log('Seems STM');
 	//	console.log(device);
 		if(action === 'add') {
-			deviceAdd(device);
+			 setTimeout(function() {
+				console.log('Toggling deviceAdd');	
+				deviceAdd(device);
+			}, 1000);
 		}
 	}
 }
-var globalUsbDev = [];
+
+reset();
+
+statusLed.blink(4, 100);
+errorLed.blink(4, 100);
+
 //Setting button click event
 button.onClick(function(state){
-	//Toggling USB device
+	//Toggling USB devic
+	reset();
 	console.log('Toggling usb');
 	exec('echo 1-1 > /sys/bus/usb/drivers/usb/unbind && echo 1-1 > /sys/bus/usb/drivers/usb/bind');
 });
@@ -43,20 +75,20 @@ var getBin = function(pattern, path) {
 	if(!path) {
 		path = '/media/usb0';
 	}
-
+	console.log('reading files in '+path);
 	var files = fs.readdirSync(path);
+	console.log(files);
     for (var i in files) {
 		if(!files[i].match(pattern)) {
 			continue;
 		}
 
-        var name = dir + '/' + files[i];
+        var name = path + '/' + files[i];
         
 		if (!fs.statSync(name).isDirectory()){
             return name;
         }
     }
-	
 	return null;
 }
 var getFlashFile = function(version) {
@@ -90,7 +122,7 @@ var deviceAdd = function(device) {
 		console.log('Checking STM32 device');
 
 		if(!isSTM32()) {
-			console.log('Seems STM8 - not yet supported');
+			error('Seems STM8 - not yet supported');
 			return null;
 		}
 		
@@ -99,25 +131,24 @@ var deviceAdd = function(device) {
 		var flashFile = getFlashFile('32');
 		
 		if(!flashFile) {
+			error('Firmware files not found (usb not attached??)');
 			return null;
 		}	
 
-		led.set(1);
+		statusLed.set(1);
 
 		var ocd = exec("openocd -f "+cdr+"/openocd/stm32_linked.cfg -c \"flash_image "+flashFile+"\"");
 
 		if(ocd.code !== 0) {
-			led.blink(4, 1000);
-			console.log('Failed to flash STM ');
+			errorg('Failed to flash STM ');
 			console.log(ocd);
 		}else{
-			led.blink(8, 250);
-			console.log('Successfully wrote: ', flashFile);
+			success('Successfully wrote: ' + flashFile);
 		}
 	    	
 
 	}catch(e){
-		console.log(e);
+		error(e);
 	}
 
 }
